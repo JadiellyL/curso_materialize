0f203d7e1c0d03265939946abb83b99eb7df8926
JadiellyL <m.jadiellydlima@gmail.com>
2022-08-16 18:51:36 -0300

    aula04 Navbar (Dropdown e Sidenav)

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const floatingBtn = M.FloatingActionButton.init(elementsBtns,{
     direction: "top",
     hoverEnabled: false
 });
+
+//navbar
+const elementsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instancesDropdown = M.Dropdown.init(elementsDropdown,{
+    coverTrigger: false
+});
+elemtsSidenav = document.querySelectorAll(".sidenav");
+const instancesSidenav = M.Sidenav.init(elemtsSidenav);
